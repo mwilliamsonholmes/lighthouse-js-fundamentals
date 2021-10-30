54da3b488a94a8e0e7df700f00ab205447c48016
Melissa <mawilliamson10@gmail.com>
2021-10-30 23:40:36 +0000

    checks if odd number

---
+++ odd.js
@@ -0,0 +1,7 @@
+const isOdd = function (num) {
+return num % 2 !== 0;
+}
+console.log(isOdd(2));
+console.log(isOdd(3));
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
