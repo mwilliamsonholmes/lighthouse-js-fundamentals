6f331949d31ecb44d00f5e8ea849418855cc1c8b
Melissa <mawilliamson10@gmail.com>
2021-10-30 23:32:21 +0000

    Checking if a number is even

---
+++ even.js
@@ -0,0 +1,9 @@
+const isEven = function (num) {
+  return num % 2 === 0;
+}
+// const tenIsEven = isEven(10);
+// const elevenIsEven = isEven(11);
+// console.log(tenIsEven);
+// console.log(elevenIsEven);
+console.log(isEven(10));
+console.log(isEven(11));
