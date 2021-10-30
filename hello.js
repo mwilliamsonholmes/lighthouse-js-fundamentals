abd247b194b1e96e53d2f9eea95979f4f6b36eaf
Melissa <mawilliamson10@gmail.com>
2021-10-30 23:16:39 +0000

    created function

---
+++ hello.js
@@ -0,0 +1,10 @@
+// const sayHello = function () {
+//   console.log("Hello, world");
+// }
+// sayHello();
+const sayHello = function (name) {
+  console.log("Hello, " + name);
+}
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
