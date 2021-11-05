9fe5f3d039fe36b03bdf42efa6bbda5f96341a2e
Melissa <mawilliamson10@gmail.com>
2021-11-05 05:52:23 +0000

    items in a packing list

---
+++ packing-list.js
@@ -0,0 +1,6 @@
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kitchen stuff to pack:");
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
